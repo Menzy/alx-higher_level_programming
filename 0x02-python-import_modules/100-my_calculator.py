a4f63b64ddbd7b24e620f63f9d250a4701e7e45b
Menzy <joshuamensah46@gmail.com>
2024-01-18 21:02:21 +0000

    fixed errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
 
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    # Check if the correct number of command-line arguments is provided
+    if len(argv) - 1 != 3:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    # Perform the calculation based on the provided operator
+    if argv[2] == '+':
+        print("{} + {} = {:d}"
+              .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
 
-    if operator not in ['+', '-', '*', '/']:
+    elif argv[2] == '-':
+        print("{} - {} = {:d}"
+              .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+
+    elif argv[2] == '*':
+        print("{} * {} = {:d}"
+              .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+
+    elif argv[2] == '/':
+        print("{} / {} = {:d}"
+              .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-    if operator == '+':
-        result = a + b
-    elif operator == '-':
-        result = a - b
-    elif operator == '*':
-        result = a * b
-    elif operator == '/':
-        if b == 0:
-            print("Error: Division by zero.")
-            exit(1)
-        result = a / b
-
-    print(f"{a} {operator} {b} = {result}")
