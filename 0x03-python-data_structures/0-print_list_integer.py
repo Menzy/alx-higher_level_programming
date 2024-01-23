3ebe0bb20a2ed6e466ae4aa674f1c1a10021f6d1
Menzy <joshuamensah46@gmail.com>
2024-01-23 00:32:15 +0000

    Fixed errors

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=None):
-    if my_list is None:
-        my_list = []
 
+def print_list_integer(my_list=[]):
+    """
+    Prints each integer in the provided list.
+
+    Parameters:
+        my_list (list): List of integers to be printed.
+    """
     for num in my_list:
         print("{:d}".format(num))
