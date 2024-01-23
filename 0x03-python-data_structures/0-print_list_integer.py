341d509c3cc4f441aaff676a961712c1a3a7e7b0
Menzy <joshuamensah46@gmail.com>
2024-01-23 00:35:00 +0000

    fixed error

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,7 +5,7 @@ def print_list_integer(my_list=[]):
     Prints each integer in the provided list.
 
     Parameters:
-        my_list (list): List of integers to be printed.
+        my_list (list): List of integers to be printed
     """
     for num in my_list:
         print("{:d}".format(num))
